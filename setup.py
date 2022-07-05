106e5939983dbf34e0f0c7837784cf705cbcee9d
Artem Pereverzev <Artem_Pereverzev@jabil.com>
2022-07-05 19:08:55 +0200

    Updated requirements versions

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'boto3>=1.4.4',
-        'botocore>=1.5.7',
-        'click>=6.6',
-        'docutils>=0.13.1',
-        'futures>=3.0.5',
-        'jmespath>=0.9.1',
+        'boto3>=1.24.20',
+        'botocore>=1.27.20',
+        'click>=8.1.3',
+        'docutils>=0.18.1',
+        'futures3>=1.0.0',
+        'jmespath>=1.0.1',
         'retrying>=1.3.3',
-        's3transfer>=0.1.10',
-        'six>=1.10.0'
+        's3transfer>=0.6.0',
+        'six>=1.16.0'
     ],
     entry_points={
         "console_scripts": [
